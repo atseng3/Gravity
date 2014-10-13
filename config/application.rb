ec1ef524311e1de76e7ca8630153f769076f8cfe
Albert Tseng <albert@chegg.com>
2014-10-13 16:21:53 -0700

    added devise to user registration

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module Gravity
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    
+    # this is for Heroku, do not compile assets on initialize 
+    config.assets.initialize_on_precompile = false
+    
     # config.i18n.default_locale = :de
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Gravity::Application.routes.draw do
+  devise_for :users
+  resources :users
+  root :to => "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
