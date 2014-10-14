087c169c61f72245d0cd7b9e955645f4478afa54
Albert Tseng <atseng3@gmail.com>
2014-10-13 17:17:39 -0700

    add some default actions to controllers

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,37 @@
 class SubscriptionsController < ApplicationController
+  
+  # GET /subscriptions
+  # GET /subscriptions.json
+  def index
+    @subscriptions = Subscription.all
+    render :index
+  end
+  
+  # GET /subscriptions/1
+  # GET /subscriptions/1.json
+  def show
+  end
+  
+  # GET /subscriptions/new
+  def new
+  end
+
+  # GET /subscriptions/1/edit  
+  def edit
+  end
+  
+  # POST /subscriptions
+  # POST /subscriptions.json  
+  def create
+  end
+  
+  # PATCH/PUT /subscriptions/1
+  # PATCH/PUT /subscriptions/1.json  
+  def update
+  end
+  
+  # DELETE /subscriptions/1
+  # DELETE /subscriptions/1.json  
+  def destroy
+  end
 end
